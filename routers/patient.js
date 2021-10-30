56ceca8f90f21aab311c8e28caff53d5185639ff
gauravk403@gmail.com <gauravk403@gmail.com>
2021-10-31 00:42:47 +0530

    doctor ui modified

--- routers/patient.js
+++ routers/patient.js
@@ -10,34 +10,33 @@ const Prescription = require("../models/prescription");
 
 const { forwardAuthenticated, ensureAuthenticated } = require("../config/auth");
 
-router.get('/prescriptions',ensureAuthenticated,async (req, res) => {
+router.get('/prescriptions', ensureAuthenticated, async (req, res) => {
     console.log("IN patient prescriptions")
-    var prescriptions=await Prescription.find({owner:req.user._id},{date:1,_id:0,medicines:1})
+    var prescriptions = await Prescription.find({ owner: req.user._id }, { date: 1, _id: 0, medicines: 1 })
     console.log(prescriptions);
     res.render("patient/prescriptions",
-    {
-        prescriptions,
-        user:req.user
-    })
+        {
+            prescriptions,
+            user: req.user
+        })
 })
 
-router.get('/dashboard',ensureAuthenticated,async(req,res)=>
-{
+router.get('/dashboard', ensureAuthenticated, async (req, res) => {
     res.render("patient/dashboard",
-    {
-        user:req.user
-    });
+        {
+            user: req.user
+        });
 })
 
-router.get('/profile',ensureAuthenticated,async (req, res) => {
+router.get('/profile', ensureAuthenticated, async (req, res) => {
     console.log("IN patient profile")
-    var patient=await Patient.find({email:req.user.email})
+    var patient = await Patient.find({ email: req.user.email })
     console.log(patient);
     res.render("patient/profile",
-    {
-        patient,
-        user:req.user
-    })
+        {
+            patient,
+            user: req.user
+        })
 })
 
 module.exports = router;
